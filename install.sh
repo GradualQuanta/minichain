9bbf1cf582950e3a99715e8319dc7de7ca72f1b3
Michel G. Combes <michelcombes@advancementofcivilizationeffort.org>
2019-12-11 10:22:31 +0100

    new install script

--- install.sh
+++ install.sh
@@ -2,18 +2,19 @@
 
 # installation [script](https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/_data/install.yml)
 
+export BRNG_HOME=$HOME/.brings
 # 1. INSTALLING LOCAL PERL MODULES ...
 # ------------------------------------
 
-echo running _perl5/boostrap.sh
-curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/_perl5/bootstrap.sh | sh /dev/stdin
+echo running .brings/perl5/boostrap.sh
+curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/.brings/perl5/bootstrap.sh | sh /dev/stdin
 
-echo running bin/install_modules.sh
-curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/bin/install_modules.sh | sh /dev/stdin
+echo running .brings/perl5/install_modules.sh
+curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/.brings/perl5/install_modules.sh | sh /dev/stdin
 
 
 # 2. INSTALLING IPFS ...
 # ----------------------
 
-echo _ipfs/bin/bootstrap.sh
-curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/_ipfs/bin/bootstrap.sh | sh /dev/stdin
+echo .brings/ipms/bin/bootstrap.sh
+curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/.brings/ipms/bin/bootstrap.sh | sh /dev/stdin
