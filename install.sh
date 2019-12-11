2bf7276ac39a4be1fea2a13118364e4be153dc58
Michel G. Combes <michelcombes@advancementofcivilizationeffort.org>
2019-12-11 15:52:21 +0100

    the 3 new initial scripts

--- install.sh
+++ install.sh
@@ -3,19 +3,36 @@
 # installation [script](https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/_data/install.yml)
 
 export BRNG_HOME=$HOME/.brings
+export IPMS_HOME=$HOME/.imps
+export IPFS_PATH=$HOME/.brings/ipfs
+export PERL5LIB=$HOME/.brings/perl5/lib/perl5
+
 # 1. INSTALLING IPFS ...
 # ----------------------
 
-echo .brings/ipms/bin/bootstrap.sh
-curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/.brings/ipms/bin/bootstrap.sh | sh /dev/stdin
+echo $IPMS_HOME/bin/bootstrap.sh
+curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/.ipms/bin/bootstrap.sh | sh /dev/stdin
 
-# 2. INSTALLING LOCAL PERL MODULES ...
+echo starting daemon
+curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/start.sh | sh /dev/stdin
+
+# 2. SETTING BRNG ENVIRONMENT ...
+# --------------------------------
+
+# ipfs add -r -Q $PROJDIR/.brings/bootstrap/bin/envrc.sh
+eval "$($IPFS_PATH/bin/ipms cat QmP4GqXnZtcNUQF94Qx3UCynQKoMtcAEA5HfQ8FZ4Jkw9i)"
+
+# 3. INSTALLING LOCAL PERL MODULES ...
 # ------------------------------------
 
-echo running .brings/perl5/boostrap.sh
-curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/.brings/bootstrap/perl5/install-local-lib.sh | sh /dev/stdin
+echo installing ${PERL5LIB%/lib/perl}
 
-echo running .brings/perl5/install_modules.sh
-curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/.brings/bootstrap/perl5/install_modules.sh | sh /dev/stdin
+echo running /.brings/boostrap/perl5/install-local-lib.sh
+ipms files read /.brings/bootstrap/perl5/install-local-lib.sh | sh /dev/stdin
+#curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/.brings/bootstrap/perl5/install-local-lib.sh | sh /dev/stdin
+
+echo running /.brings/bootstrap/perl5/install_modules.sh
+ipms files read /.brings/bootstrap/perl5/install_modules.sh | sh /dev/stdin
+#curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/.brings/bootstrap/perl5/install_modules.sh | sh /dev/stdin
 
 
--- rc.sh
+++ rc.sh
@@ -1,5 +1,8 @@
 
+# ipfs add -r -Q $PROJDIR/.brings/bootstrap/bin/envrc.sh
+envrc=QmP4GqXnZtcNUQF94Qx3UCynQKoMtcAEA5HfQ8FZ4Jkw9i
 export BRNG_HOME=${BRNG_HOME:-$HOME/.brings}
 export IPFS_PATH=${IPFS_PATH:-$BRNG_HOME/ipms}
-eval "$($IPFS_PATH/bin/ipms cat QmVGvrZJoSzs6HLNB44SFQjaV9Xw5GvJ7bfjucyQRMYhsv)"
+export PERL5LIB=${PERL5LIB:-$BRNG_HOME/perl5/lib/perl5}
+eval "$($IPFS_PATH/bin/ipms cat /ipfs/$envrc)"
 
