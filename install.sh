ff3971e57581444248202a782fae337ebc7a0ee5
Michel G. Combes <michelcombes@advancementofcivilizationeffort.org>
2019-12-11 10:46:25 +0100

    move perl's bootstrap

--- install.sh
+++ install.sh
@@ -3,18 +3,19 @@
 # installation [script](https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/_data/install.yml)
 
 export BRNG_HOME=$HOME/.brings
-# 1. INSTALLING LOCAL PERL MODULES ...
-# ------------------------------------
-
-echo running .brings/perl5/boostrap.sh
-curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/.brings/perl5/bootstrap.sh | sh /dev/stdin
-
-echo running .brings/perl5/install_modules.sh
-curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/.brings/perl5/install_modules.sh | sh /dev/stdin
-
-
-# 2. INSTALLING IPFS ...
+# 1. INSTALLING IPFS ...
 # ----------------------
 
 echo .brings/ipms/bin/bootstrap.sh
 curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/.brings/ipms/bin/bootstrap.sh | sh /dev/stdin
+
+# 2. INSTALLING LOCAL PERL MODULES ...
+# ------------------------------------
+
+echo running .brings/perl5/boostrap.sh
+curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/.brings/bootstrap/perl5/install-local-lib.sh | sh /dev/stdin
+
+echo running .brings/perl5/install_modules.sh
+curl -s https://raw.githubusercontent.com/Gradual-Quanta/minichain/master/.brings/bootstrap/perl5/install_modules.sh | sh /dev/stdin
+
+
