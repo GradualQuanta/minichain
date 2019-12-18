525a6ff66ee86f0dd1dd01fe28810a4def295afa
Michel G. Combes <michelcombes@advancementofcivilizationeffort.org>
2019-12-18 16:04:59 +0100

    added addfriend and online script

--- mfs/.brings/bin/ipmsd.sh
+++ mfs/.brings/bin/ipmsd.sh
@@ -5,7 +5,12 @@ yellow="[33m"
 red="[31m"
 nc="[0m"
 
+if echo $0 | grep -q '/'; then
 bindir=${0%/*}
+else
+bindir=.
+fi
+
 PATH=$bindir:$PATH
 if [ "x$DICT" = 'x' ]; then
 export DICT=$bindir/../etc
@@ -17,19 +22,24 @@ fi
 #qm=$(echo "IPFS is active" | ipms add --offline -Q) && echo "qm: $qm"
 which ipms
 
-export IPFS_PATH=${IPFS_PATH:=$BRNG_HOME/ipfs}
+#export IPFS_PATH=${IPFS_PATH:=$BRNG_HOME/ipfs}
 if ! ipms swarm addrs local 2>/dev/null; then
   echo "${red}WARNING no ipms daemon running${nc}, starting one !"
   OPTIONS="--unrestricted-api --enable-namesys-pubsub"
-  pp=$(cat $IPFS_PATH/config | xjson Addresses.Gateway | cut -d'/' -f 5)
-  peerid=$(cat $IPFS_PATH/config | xjson Identity.PeerID)
+  if [ "x$IPFS_PATH" != 'x' ]; then
+    pp=$(cat $IPFS_PATH/config | xjson Addresses.Gateway | cut -d'/' -f 5)
+    peerid=$(cat $IPFS_PATH/config | xjson Identity.PeerID)
+  else
+    pp=$(ipms --offline config Addresses.Gateway | cut -d'/' -f 5)
+    peerid=$(ipms --offline config Identity.PeerID)
+  fi
   name=$(perl -S fullname.pl $peerid)
 
   if which rxvt 1>/dev/null; then
-    rxvt -geometry 128x18 -bg black -fg orange -name IPFS -n "$pp" -title "ipms daemon:$pp ($IPFS_PATH) ~ $name" -e ipms daemon $OPTIONS &
+    rxvt -geometry 128x18 -bg black -fg orange -name IPFS -n "$pp" -title "ipms daemon:$pp (${IPFS_PATH:-~/.ipfs}) ~ $name" -e ipms daemon $OPTIONS &
   else
     if which xterm 1>/dev/null; then
-      xterm -geometry 128x18 -bg black -fg orange -name IPFS -n "$pp" -title "ipms daemon:$pp ($IPFS_PATH) ~ $name" -e ipms daemon $OPTIONS &
+      xterm -geometry 128x18 -bg black -fg orange -name IPFS -n "$pp" -title "ipms daemon:$pp (${IPFS_PATH:-~/.ipfs}) ~ $name" -e ipms daemon $OPTIONS &
     else
       gnome-terminal --title "ipms daemon:$pp ($IPFS_PATH) ~ $name" -- ipms daemon $OPTIONS &
     fi
