cc48ea402fd29217f6cff44157238ed679770a4b
Michel G. Combes <testing@minichain.ml>
2019-12-18 17:47:52 +0100

    added perl dependency

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ export IPMS_HOME="${IPMS_HOME:-$HOME/.ipms}"
 
 export PATH="$(pwd)/bin:$IPMS_HOME/bin:$PATH"
 perl -S dot2yml deps.dot 
+dot deps.dot -Tpng -o deps.png; eog deps.png &
+
 perl -S ymake $1 deps.yml minichain
 
 # $Source: /my/tools/blockchains/minichain/install.sh$
