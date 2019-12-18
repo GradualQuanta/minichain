c4385979fd693f6ddd8dc5830bb5aae3e4f285e6
Michel G. Combes <michelcombes@advancementofcivilizationeffort.org>
2019-12-18 16:16:52 +0100

    typo !

--- mfs/.brings/bootstrap/bin/install_binetc.sh
+++ mfs/.brings/bootstrap/bin/install_binetc.sh
@@ -2,7 +2,7 @@
 
 BRNG_HOME=${BRNG_HOME:-$HOME/.brings}
 qm=$(ipms files stat --hash /.brings/bin)
-chmod a+x /.brings/bin/*
 ipms get $qm -o $BRNG_HOME/bin
+chmod a+x $BRNG_HOME/.brings/bin/*
 qm=$(ipms files stat --hash /.brings/etc)
 ipms get $qm -o $BRNG_HOME/etc
