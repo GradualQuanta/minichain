9a9a654be37936f64342a4c3a8844feac5163a1e
Michel G. Combes <michelcombes@advancementofcivilizationeffort.org>
2019-12-18 18:07:40 +0100

    added which dot !

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ export IPMS_HOME="${IPMS_HOME:-$HOME/.ipms}"
 
 export PATH="$(pwd)/bin:$IPMS_HOME/bin:$PATH"
 perl -S dot2yml deps.dot 
+if which dot; then
 dot deps.dot -Tpng -o deps.png; eog deps.png &
+fi
 
 perl -S ymake $1 deps.yml minichain
 
