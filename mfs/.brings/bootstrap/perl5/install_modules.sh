ab4f5837bb987feafc431e001e0338631baaa42a
Michel C <michelc@gc-bank.org>
2019-12-20 14:36:36 +0100

    put install module JSON at the end

--- mfs/.brings/bootstrap/perl5/install_modules.sh
+++ mfs/.brings/bootstrap/perl5/install_modules.sh
@@ -12,9 +12,9 @@ export PATH="${rootdir}/bin:$PATH"
 # perl's local lib
 eval $(perl -I$rootdir/perl5/lib/perl5 -Mlocal::lib=$rootdir/perl5)
 
-perl -MCPAN -Mlocal::lib=$rootdir/perl5 -e 'CPAN::install(JSON)'
 perl -MCPAN -Mlocal::lib=$rootdir/perl5 -e 'CPAN::install(YAML::Syck)'
 perl -MCPAN -Mlocal::lib=$rootdir/perl5 -e 'CPAN::install(MIME::Base32)'
 perl -MCPAN -Mlocal::lib=$rootdir/perl5 -e 'CPAN::install(Math::BigInt)'
 perl -MCPAN -Mlocal::lib=$rootdir/perl5 -e 'CPAN::install(Encode::Base58::BigInt)'
+perl -MCPAN -Mlocal::lib=$rootdir/perl5 -e 'CPAN::install(JSON)'
 
